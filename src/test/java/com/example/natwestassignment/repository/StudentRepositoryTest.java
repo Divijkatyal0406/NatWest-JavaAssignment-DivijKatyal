37bda92663d12a8864f1bf03c5ad5e31bc8239a7
Divij Katyal <divijkatyal@gmail.com>
2024-02-16 04:39:09 +0530

    addtional-tests

--- src/test/java/com/example/natwestassignment/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/natwestassignment/repository/StudentRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.example.natwestassignment.repository;
 
 import com.example.natwestassignment.model.Student;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -19,5 +21,14 @@ public class StudentRepositoryTest {
         student.setRollNumber(1L);
         student.setEligible("YES");
         repo.save(student);
+    }
+
+    @Test
+    void shouldFindStudentByRollNumber() {
+        Long rollNumber = 1L;
+        Student foundStudent = repo.findById(rollNumber).orElse(null);
+        Assertions.assertNotNull(foundStudent);
+        Assertions.assertEquals(rollNumber, foundStudent.getRollNumber());
+        Assertions.assertEquals("YES", foundStudent.isEligible());
     }
 }
